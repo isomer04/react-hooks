3b2f234cf2b5526ce35844848c7857cf9a19f163
isomer04 <isomer008@gmail.com>
2022-12-20 18:30:43 -0500

    useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { FaStar } from "react-icons/fa";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
+}
+function App() {
+  return <StarRating totalStars={6} />;
+}
+
+// function App() {
+//   const [status, setStatus] = useState("Not Delivered");
+//   const [checked, setChecked] = useState(false);
+
+//   return (
+//     <div>
+//       <input type="checkbox" value={checked} onChange={(checked) => setChecked(!checked)} />
+
+//       <p>{checked ? "checked" : "not checked"} </p>
+
+//       {/* <h1> The package is: {status}.</h1>
+//       <button onClick={() => setStatus("Delivered")}>Deliver</button> */}
+//     </div>
+//   );
+// }
+
+const [, , third] = ["Alex", "Ali", "Anna"];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
